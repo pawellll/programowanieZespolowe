37ee8093797ce6ac205548fa4f06baa76ba60e99
Kamil Nowak <kamil.nowak.930527@gmail.com>
2017-06-01 22:10:39 +0200

    temp

--- credentialsService/credentialsService.js
+++ credentialsService/credentialsService.js
@@ -45,26 +45,128 @@ credentialsService.get('/authenticate', function(req, res) {
 	res.header('Access-Control-Allow-Origin', '*');
 
 	var credentials = auth(req);
-
-	var params = {};
-
-	var credentials = auth(req)
 	
-	var username = credentials.name;
-	var password = credentials.pass;
-	
-	winston.info("starting authentication for user " + username);
-	winston.info("password: " + password);
-
-	if (!credentials || credentials.name !== 'test' || credentials.pass !== 'test') {
-		res.statusCode = 401
-		res.end('Access denied')
+	if (checkCredentials(credentials)) {
+		res.status(200);
+		res.end('Access granted');
 	} else {
-		res.end('Access granted')
+		res.status(401);
+		res.end('Access denied');
 	}
-	var result = {};
-	res.status(200).json(result);	
-
+	
 });
+
+credentialsService.post('/createUser', function(req, res) {
+	res.header('Access-Control-Allow-Origin', '*');
+
+	var credentials = auth(req);
+	if (checkCredentials(credentials) && validUser(req.body) ) {
+		createUser(req.body, res);
+		res.status(201).send();
+	} else {
+		winston.info("Cannot create user");
+		res.status(400).send();
+	}
+});
+
+
+credentialsService.get('/users', function(req, res) {
+	var credentials = auth(req);
+	if (checkCredentials(credentials)) {
+		UserModel.find({}, function(err, users) {
+			if (err) {
+				winston.error("Error while getting users: " + err);
+				res.status(500).send();
+				return;
+			}
+			var result = {
+				users : []
+			};
+			for (var i = 0; i < result.length; ++i) {
+				result.users.push({
+					username : users[i].username
+				});
+			}
+			res.status(200).json(result);
+		});
+	} else {
+		res.status(401);
+		res.end('Access denied');
+	}
+});
+
+
+function checkCredentials(credentials) {
+	if(credentials && credentials.name === 'test' && credentials.pass === 'test') {
+		winston.info("Starting authentication for user " + credentials.name);
+		return true;
+	} else {
+		winston.info("Invaild credentials");
+		return false;
+	}
+}
+
+var validUserResult;
+function validUser(body) {
+	validUserResult = 'undefined';
+	if(body.username && body.password) {
+		checkIfUserExists(body.username, function(out) {return !out;/*winston.info("out " + out)*/;});
+//		while(validUserResult === 'undefined') {
+//			sleep(1000);
+//			winston.info("sleeping...");
+//		} 
+		winston.info("LOG " + validUserResult);
+		//return !validUserResult;
+	} else {
+		return false;
+	}
+}
+
+function sleep(milliseconds) {
+	  var start = new Date().getTime();
+	for (var i = 0; i < 1e7; i++) {
+		if ((new Date().getTime() - start) > milliseconds) {
+			break;
+		}
+	}
+}
+
+function checkIfUserExists(user, callback) {
+	UserModel.findOne({
+		username : user
+	}, function(err, metadata) {
+		if (!metadata) {
+			winston.info("User " + user + " doesnot exist");
+			callback(complete(false));
+		} else {
+			var result = metadata.username;
+			winston.info("User " + result + " exists");
+			callback(complete(true));
+		}
+	});	
+	function complete(value) {
+		winston.info("LOG comp " + value);
+		return value;
+	}
+}
+
+
+function createUser(body, res) {
+	var data = {};
+
+	data.username = body.username;
+	data.password = body.password;
+
+	UserModel.create(data, function(err, metadata) {
+		if (err) {
+			winston.error("Error while user creating: " + err);
+			res.status(500).send();
+			return;
+		} else {
+			winston.info("User " + metadata.username + " created");
+			res.status(201).send();
+		}
+	});
+}
 
 credentialsService.listen(config.httpPort);
